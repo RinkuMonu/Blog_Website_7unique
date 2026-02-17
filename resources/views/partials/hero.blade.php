148c5b0fe67ad2acda6407c0940364f696bdde26
niranjan <niranjan@7unique.in>
2026-02-17 15:45:23 +0530

    updated header

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,22 +1,40 @@
-<div class="text-center mb-5">
-    <div class="col-md-8 mx-auto">
-        <a href="/">
-            <img src="{{ asset('images/add1.png') }}" alt="Advertisement" class="img-fluid">
-        </a>
-        <p class="text-uppercase small text-muted mt-3 mb-4">
-            Advertisement
-        </p>
+<!-- Top Advertisement -->
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-11 col-md-10 col-lg-8 text-center">
+
+            <a href="/">
+                <img src="{{ asset('images/add1.png') }}"
+                     alt="Advertisement"
+                     class="img-fluid w-100"
+                     style="max-height:250px; object-fit:contain;">
+            </a>
+
+            <p class="text-uppercase small text-muted mt-3 mb-0">
+                Advertisement
+            </p>
+
+        </div>
     </div>
 </div>
 
 
 <!-- Bottom Advertisement -->
-<div class="text-center mt-5">
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-11 col-md-10 col-lg-8 text-center">
 
-    <a href="/">
-        <img src="{{ asset('images/add2.png') }}" alt="Advertisement" class="img-fluid mx-auto d-block">
-    </a>
-    <p class="text-uppercase small text-muted mb-5 mt-5">
-        Advertisement
-    </p>
+            <a href="/">
+                <img src="{{ asset('images/add2.png') }}"
+                     alt="Advertisement"
+                     class="img-fluid w-100"
+                     style="max-height:250px; object-fit:contain;">
+            </a>
+
+            <p class="text-uppercase small text-muted mt-3 mb-0">
+                Advertisement
+            </p>
+
+        </div>
+    </div>
 </div>
